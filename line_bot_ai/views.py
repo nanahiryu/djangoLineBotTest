a7d1ef669bc9875f3ec3d36d22f9b46b9d8e1b29
nanahiryu <ryu2751keio@gmail.com>
2022-09-04 22:07:10 +0900

    buttonを実装してみた

--- line_bot_ai/views.py
+++ line_bot_ai/views.py
@@ -1,11 +1,12 @@
 from email.mime import image
+from re import template
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
 
 from linebot import LineBotApi
-from linebot.models import ImageSendMessage
+from linebot.models import ImageSendMessage, TextSendMessage, TemplateSendMessage, ButtonsTemplate
 from linebot.exceptions import LineBotApiError
 
 import environ
@@ -21,17 +22,49 @@ def index(request):
     if request.method == 'POST':
         request = json.loads(request.body.decode('utf-8'))
         data = request['events'][0]
-        message = data['message']['text']
+        print(data)
         reply_token = data['replyToken']
-        if message == '正解':
-            res_image = ImageSendMessage(original_content_url=r"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGwjQL1IJHjHO4p3Z16VSdNsVRzrTvIcbNW4FVLJgnoe7ZiACh&s", preview_image_url=r"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGwjQL1IJHjHO4p3Z16VSdNsVRzrTvIcbNW4FVLJgnoe7ZiACh&s")
-        else:
-            res_image = ImageSendMessage(original_content_url=r"https://2.bp.blogspot.com/-b8RGYZ3IXX4/Vf-aJng1qzI/AAAAAAAAyDo/pTdU_3xTK0w/s400/dame_man.png", preview_image_url=r"https://2.bp.blogspot.com/-b8RGYZ3IXX4/Vf-aJng1qzI/AAAAAAAAyDo/pTdU_3xTK0w/s400/dame_man.png")
+        if 'message' in data.keys():
+            message = data['message']['text']
+            if message == '画像':
+                res_context = ImageSendMessage(original_content_url=r"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGwjQL1IJHjHO4p3Z16VSdNsVRzrTvIcbNW4FVLJgnoe7ZiACh&s", preview_image_url=r"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGwjQL1IJHjHO4p3Z16VSdNsVRzrTvIcbNW4FVLJgnoe7ZiACh&s")
+            elif message == '選択肢':
+                # 選択肢と送られるとボタンのテンプレートが出せます
+                # labelがユーザーから見えている文字列, dataがそのボタンが押されたときにサーバーに返ってくる文字列になります
+                actions = [
+                    {
+                        "type": "postback",
+                        "label": "山",
+                        "data": "event=journey&action=mountain"
+                    },
+                    {
+                        "type": "postback",
+                        "label": "海",
+                        "data": "event=journey&action=ocean"
+                    },
+                ]
+                res_context = TemplateSendMessage(
+                    alt_text='buttons template',
+                    template=ButtonsTemplate(text='旅行にいくならどっちがいい...？', actions=actions)
+                    )
+            else:
+                res_context = TextSendMessage(text="それには答えられないわ...")
+                
+        # buttonのresponse.dataのjsonには'postback'というkeyが含まれます
+        # そのためbuttonへの返答には以下の文が動きます
+        if 'postback' in data.keys():
+            query = {}
+            temp_list = data['postback']['data'].split('&')
+            for temp in temp_list:
+                key, item = temp.split('=')
+                query[key] = item
+            if data['postback']['data'] == 'event=journey&action=mountain':
+                res_context = TextSendMessage(text=f'山楽しいよね event: {query["event"]} action: {query["action"]}')
+            if data['postback']['data'] == 'event=journey&action=ocean':
+                res_context = TextSendMessage(text=f'海楽しいよね event: {query["event"]} action: {query["action"]}')
 
         try:
-            line_bot_api.reply_message(reply_token, res_image)
-            print(message)
-            print(res_image)
+            line_bot_api.reply_message(reply_token, res_context)
         except LineBotApiError as e:
             print(e)
 
